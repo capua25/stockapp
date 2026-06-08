bd4f2851fbbf5350b8defcdcaf8494a18be451b1
Martin <capuano.martin@outlook.com>
2026-06-08 14:56:57 -0300

    chore: app Avalonia MVVM referenciando Application e Infrastructure

---
+++ src/StockApp.Presentation/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace StockApp.Presentation.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ src/StockApp.Presentation/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace StockApp.Presentation.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
