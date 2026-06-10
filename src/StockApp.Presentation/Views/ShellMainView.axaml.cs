d7510ad63898180a70697c22771cb947f37fcb37
Martin <capuano.martin@outlook.com>
2026-06-09 21:44:28 -0300

    feat(presentation): ShellMainViewModel con menú lateral por rol

---
+++ src/StockApp.Presentation/Views/ShellMainView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace StockApp.Presentation.Views;
+
+public partial class ShellMainView : UserControl
+{
+    public ShellMainView()
+    {
+        InitializeComponent();
+    }
+}
