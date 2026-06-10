00afa82cd5d7f364940021bd955bd877292d5be4
Martin <capuano.martin@outlook.com>
2026-06-09 21:44:52 -0300

    feat(presentation): UnidadMedidaListViewModel, UnidadMedidaFormViewModel y vistas

---
+++ src/StockApp.Presentation/Views/Catalogo/UnidadMedidaFormView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace StockApp.Presentation.Views.Catalogo;
+
+public partial class UnidadMedidaFormView : UserControl
+{
+    public UnidadMedidaFormView()
+    {
+        InitializeComponent();
+    }
+}
